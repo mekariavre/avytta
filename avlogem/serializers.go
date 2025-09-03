2bfd35051ea4cd496eba3f81b2884b0d157e4030
Avre Barra <avreghly.barra@mekari.com>
2025-09-03 16:39:16 +0700

    avlogem: fix serializer json piping and add docs

--- avlogem/serializers.go
+++ avlogem/serializers.go
@@ -16,6 +16,19 @@ type HTTPResponseData struct {
 }
 
 // SerializeHTTPResponse extracts status, headers, and body from *http.Response without consuming the body.
+//
+// It extracts the body as a string, and tries to parse it as JSON. If the parse fails, it sets the Body field
+// to the original string.
+// SerializeHTTPResponse reads and serializes an http.Response into an HTTPResponseData struct.
+// It safely reads the response body, restores it for further use, and parses the body content
+// using ParsePipeJSON. If reading the body fails, it sets the body to a placeholder error string.
+// The function also copies the response status, status code, and headers.
+//
+// Parameters:
+//   - resp: Pointer to an http.Response to be serialized.
+//
+// Returns:
+//   - HTTPResponseData: Struct containing the status, status code, headers, and parsed body of the response.
 func SerializeHTTPResponse(resp *http.Response) HTTPResponseData {
 	var bodyCopy string
 	if resp.Body != nil {
@@ -31,7 +44,7 @@ func SerializeHTTPResponse(resp *http.Response) HTTPResponseData {
 		Status:     resp.Status,
 		StatusCode: resp.StatusCode,
 		Header:     resp.Header,
-		Body:       ParsePipeJSON(bodyCopy),
+		Body:       parsejson(bodyCopy),
 	}
 }
 
@@ -44,6 +57,9 @@ type HTTPRequestData struct {
 }
 
 // SerializeHTTPRequest extracts method, url, header, and body from *http.Request without consuming the body.
+//
+// It extracts the body as a string, and tries to parse it as JSON. If the parse fails, it sets the Body field
+// to the original string.
 func SerializeHTTPRequest(req *http.Request) HTTPRequestData {
 	var bodyCopy string
 	if req.Body != nil {
@@ -59,19 +75,20 @@ func SerializeHTTPRequest(req *http.Request) HTTPRequestData {
 		Method: req.Method,
 		URL:    req.URL.String(),
 		Header: req.Header,
-		Body:   ParsePipeJSON(bodyCopy),
+		Body:   parsejson(bodyCopy),
 	}
 }
 
 // will try to parse orig as string and json and return map
 // if it fail it return original data
-func ParsePipeJSON(orig any) any {
+func parsejson(orig any) any {
+	origB, ok := orig.(string)
+	if !ok {
+		return orig
+	}
 	var m map[string]any
-	b, err := json.Marshal(orig)
-	if err == nil {
-		if err := json.Unmarshal(b, &m); err == nil {
-			return m
-		}
+	if err := json.Unmarshal([]byte(origB), &m); err == nil {
+		return m
 	}
 	return orig
 }
